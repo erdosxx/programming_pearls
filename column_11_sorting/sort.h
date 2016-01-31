2fc5364fd023886b6a247af5b15c19b196b93711
In Soo Oh <erdos.cs@gmail.com>
2016-01-31 23:09:34 +0900

    add all quick sort algorithm with testing.

--- column_11_sorting/sort.h
+++ column_11_sorting/sort.h
@@ -1,9 +1,7 @@
-//
-// Created by In Soo Oh on 1/31/16.
-//
-
 #ifndef PROGRAMMING_PEARLS_SORT_H
 #define PROGRAMMING_PEARLS_SORT_H
+
+#include <stdlib.h>
 
 void swap(int x[], int i, int j) {
     int t = x[i];
@@ -16,6 +14,109 @@ void isort1(int x[], int n) {
     for (i = 1; i < n; i++)
         for (j = i; j > 0 && x[j - 1] > x[j]; j--)
             swap(x, j - 1, j);
+}
+
+/* Write swap function inline */
+void isort2(int x[], int n) {
+    int i, j;
+    int t;
+    for (i = 1; i < n; i++)
+        for (j = i; j > 0 && x[j - 1] > x[j]; j--) {
+            t = x[j];
+            x[j] = x[j - 1];
+            x[j - 1] = t;
+        }
+}
+
+/* Move assignments to and from t out of loop */
+void isort3(int x[], int n) {
+    int i, j;
+    int t;
+    for (i = 1; i < n; i++) {
+        t = x[i];
+        for (j = i; j > 0 && x[j - 1] > t; j--)
+            x[j] = x[j - 1];
+        x[j] = t;
+    }
+}
+
+/* QUICKSORTS */
+/* Simplest version, Lomuto partitioning */
+void qsort1(int x[], int l, int u) {
+    int i, m;
+    if (l >= u)
+        return;
+    m = l;
+    for (i = l + 1; i <= u; i++)
+        if (x[i] < x[l])
+            swap(x, ++m, i);
+    swap(x, l, m);
+    qsort1(x, l, m - 1);
+    qsort1(x, m + 1, u);
+}
+
+/* Sedgewick's version of Lomuto, with sentinel */
+void qsort2(int x[], int l, int u) {
+    int i, m;
+    if (l >= u)
+        return;
+    m = i = u + 1;
+    do {
+        do i--; while (x[i] < x[l]);
+        swap(x, --m, i);
+    } while (i > l);
+    qsort2(x, l, m - 1);
+    qsort2(x, m + 1, u);
+}
+
+/* Two-way partitioning */
+void qsort3(int x[], int l, int u) {
+    int i, j;
+    int t;
+    if (l >= u)
+        return;
+    t = x[l];
+    i = l;
+    j = u + 1;
+    for (; ;) {
+        do i++; while (i <= u && x[i] < t);
+        do j--; while (x[j] > t);
+        if (i > j)
+            break;
+        swap(x, i, j);
+    }
+    swap(x, l, j);
+    qsort3(x, l, j - 1);
+    qsort3(x, j + 1, u);
+}
+
+int randint(int l, int u) {
+    //return l + (RAND_MAX * rand() + rand()) % (u - l + 1);
+    return l + (rand() % (u - l + 1));
+}
+
+/* qsort3 + randomization + isort small subarrays + swap inline */
+void qsort4(int x[], int l, int u, int cutoff) {
+    int i, j;
+    int t, temp;
+    if (u - l < cutoff)
+        return;
+    swap(x, l, randint(l, u));
+    t = x[l];
+    i = l;
+    j = u + 1;
+    for (; ;) {
+        do i++; while (i <= u && x[i] < t);
+        do j--; while (x[j] > t);
+        if (i > j)
+            break;
+        temp = x[i];
+        x[i] = x[j];
+        x[j] = temp;
+    }
+    swap(x, l, j);
+    qsort4(x, l, j - 1, cutoff);
+    qsort4(x, j + 1, u, cutoff);
 }
 
 #endif //PROGRAMMING_PEARLS_SORT_H
--- column_11_sorting_tests/basic_tests/sort_check.cpp
+++ column_11_sorting_tests/basic_tests/sort_check.cpp
@@ -8,15 +8,15 @@ protected:
 
     virtual void SetUp() {
         set_value_target_array();
-        shuffle_array(1000);
+        shuffle_array(100000);
     }
 
-    int size_ary=1000;
+    int size_ary=100000;
     int* target_array = new int[size_ary];
 
     void set_value_target_array () {
         for(int i=0; i< size_ary; i++) {
-            target_array[i] = 1000 - i;
+            target_array[i] = 100000 - i;
         }
     };
 
@@ -39,7 +39,7 @@ public:
     }
 };
 
-TEST_F(sortAlgorithmFixture, test_insertionSort) {
+TEST_F(sortAlgorithmFixture, test_insertion1Sort) {
     isort1(target_array, size_ary);
     EXPECT_EQ(1, target_array[0]);
     EXPECT_EQ(2, target_array[1]);
@@ -47,4 +47,69 @@ TEST_F(sortAlgorithmFixture, test_insertionSort) {
     EXPECT_EQ(701, target_array[700]);
     EXPECT_EQ(999, target_array[998]);
     EXPECT_EQ(1000, target_array[999]);
+}
+
+TEST_F(sortAlgorithmFixture, test_insertion2Sort) {
+    isort2(target_array, size_ary);
+    EXPECT_EQ(1, target_array[0]);
+    EXPECT_EQ(2, target_array[1]);
+    EXPECT_EQ(501, target_array[500]);
+    EXPECT_EQ(701, target_array[700]);
+    EXPECT_EQ(999, target_array[998]);
+    EXPECT_EQ(1000, target_array[999]);
+}
+
+TEST_F(sortAlgorithmFixture, test_insertion3Sort) {
+    isort3(target_array, size_ary);
+    EXPECT_EQ(1, target_array[0]);
+    EXPECT_EQ(2, target_array[1]);
+    EXPECT_EQ(501, target_array[500]);
+    EXPECT_EQ(701, target_array[700]);
+    EXPECT_EQ(999, target_array[998]);
+    EXPECT_EQ(1000, target_array[999]);
+}
+
+TEST_F(sortAlgorithmFixture, test_quick1Sort) {
+    qsort1(target_array, 0, size_ary-1);
+    EXPECT_EQ(1, target_array[0]);
+    EXPECT_EQ(2, target_array[1]);
+    EXPECT_EQ(501, target_array[500]);
+    EXPECT_EQ(701, target_array[700]);
+    EXPECT_EQ(999, target_array[998]);
+    EXPECT_EQ(10000, target_array[9999]);
+    EXPECT_EQ(100000, target_array[99999]);
+}
+
+TEST_F(sortAlgorithmFixture, test_quick2Sort) {
+    qsort2(target_array, 0, size_ary-1);
+    EXPECT_EQ(1, target_array[0]);
+    EXPECT_EQ(2, target_array[1]);
+    EXPECT_EQ(501, target_array[500]);
+    EXPECT_EQ(701, target_array[700]);
+    EXPECT_EQ(999, target_array[998]);
+    EXPECT_EQ(10000, target_array[9999]);
+    EXPECT_EQ(100000, target_array[99999]);
+}
+
+TEST_F(sortAlgorithmFixture, test_quick3Sort) {
+    qsort3(target_array, 0, size_ary-1);
+    EXPECT_EQ(1, target_array[0]);
+    EXPECT_EQ(2, target_array[1]);
+    EXPECT_EQ(501, target_array[500]);
+    EXPECT_EQ(701, target_array[700]);
+    EXPECT_EQ(999, target_array[998]);
+    EXPECT_EQ(10000, target_array[9999]);
+    EXPECT_EQ(100000, target_array[99999]);
+}
+
+TEST_F(sortAlgorithmFixture, test_quick4Sort) {
+    qsort4(target_array, 0, size_ary-1, 50);
+    isort3(target_array, size_ary);
+    EXPECT_EQ(1, target_array[0]);
+    EXPECT_EQ(2, target_array[1]);
+    EXPECT_EQ(501, target_array[500]);
+    EXPECT_EQ(701, target_array[700]);
+    EXPECT_EQ(999, target_array[998]);
+    EXPECT_EQ(10000, target_array[9999]);
+    EXPECT_EQ(100000, target_array[99999]);
 }
