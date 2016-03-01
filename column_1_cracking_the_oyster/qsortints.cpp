641f02803ed022888ffed0d731635e8240ea3f42
In Soo Oh <erdos.cs@gmail.com>
2016-03-01 23:55:37 +0900

    add column 1 source files.

---
+++ column_1_cracking_the_oyster/qsortints.cpp
@@ -0,0 +1,27 @@
+/* Copyright (C) 1999 Lucent Technologies */
+/* From 'Programming Pearls' by Jon Bentley */
+/* qsortints.c -- Sort input set of integers using qsort */
+/* problem 1 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int intcomp (const void *x, const void *y) {
+    return (*(int *)x - *(int *)y);
+}
+
+int a[1000000];
+
+int main() {
+    int i, n=0;
+
+    while (scanf("%d", &a[n]) != EOF)
+        n++;
+
+    qsort(a, n, sizeof(int), intcomp);
+
+    for (i = 0; i < n; i++)
+        printf("%d\n", a[i]);
+
+    return 0;
+}
---
+++ column_1_cracking_the_oyster/sortints.cpp
@@ -0,0 +1,23 @@
+/* Copyright (C) 1999 Lucent Technologies */
+/* From 'Programming Pearls' by Jon Bentley */
+/* sortints.cpp -- Sort input set of integers using STL set */
+/* problem 1 */
+
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    set<int> S;
+    int i;
+    set<int>::iterator j;
+
+    while (cin >> i)
+        S.insert(i);
+
+    for (j = S.begin(); j != S.end(); ++j)
+        cout << *j << "\n";
+
+    return 0;
+}
